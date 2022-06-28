ed15e11f58748a875037cc79d05653e063f011b7
Juan Rosario <jrosario19@hotmail.com>
2022-06-28 10:58:30 -0400

    Add products page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
+
+const Container = styled.div({
+
+})
+
+const Title = styled.h1({
+    margin:'20px'
+})
+
+const FilterContainer = styled.div({
+    display: 'flex',
+    justifyContent: 'space-between',
+    
+})
+
+const Filter = styled.div({
+    margin:'20px'
+})
+
+const FilterText = styled.span({
+    fontSize:'20px',
+    fontWeight: '600',
+    marginRight: '20px',
+    
+})
+
+const Select = styled.select({
+    padding:'10px',
+    marginRight: '20px',
+    
+})
+
+const Option = styled.option({
+    
+})
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Announcement/>
+      <Navbar/>
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+            <FilterText>Filter Products:</FilterText>
+            <Select>
+                <Option disabled selected>
+                    Color
+                </Option>
+                <Option>White</Option>
+                <Option>Black</Option>
+                <Option>Red</Option>
+                <Option>Blue</Option>
+                <Option>Yellow</Option>
+                <Option>Green</Option>
+            </Select>
+            <Select>
+                <Option disabled selected>
+                    Size
+                </Option>
+                <Option>XS</Option>
+                <Option>S</Option>
+                <Option>M</Option>
+                <Option>L</Option>
+                <Option>XL</Option>
+            </Select>
+        </Filter>
+        <Filter>
+            <FilterText>Sort Products:</FilterText>
+            <Select>
+                <Option selected>Newest</Option>
+                <Option>Price (asc)</Option>
+                <Option>Price (desc)</Option>
+            </Select>
+        </Filter>
+      </FilterContainer>
+      <Products/>
+      <Newsletter/>
+      <Footer/>
+    </Container>
+  )
+}
+
+export default ProductList
